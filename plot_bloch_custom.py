71222944e33ae07cad277d8d2e34f9017ce0b2f8
Oliver Beresford <oggynaut@icloud.com>
2025-11-18 10:03:18 -0500

    Add hemisphere plotting functionality and update sphere plotting logic

--- plot_bloch_custom.py
+++ plot_bloch_custom.py
@@ -16,7 +16,31 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D  # noqa: F401
 import math
 
-def plot_sphere_and_points(save_path='bloch_custom.png', show=True):
+def plot_hemisperes():
+    fig = plt.figure(figsize=(6, 6))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Create red hemisphere
+    u_red = np.linspace(0, 2 * np.pi, 120)
+    v_red = np.linspace(0, np.pi / 2, 60)
+    x_red = np.outer(np.cos(u_red), np.sin(v_red))
+    y_red = np.outer(np.sin(u_red), np.sin(v_red))
+    z_red = np.outer(np.ones_like(u_red), np.cos(v_red))
+
+    ax.plot_surface(x_red, y_red, z_red, rstride=4, cstride=4, color='red', alpha=0.35, linewidth=0)
+    
+    # Create blue hemisphere
+    u_blue = np.linspace(0, 2 * np.pi, 120)
+    v_blue = np.linspace(np.pi / 2, np.pi, 60)
+    x_blue = np.outer(np.cos(u_blue), np.sin(v_blue))
+    y_blue = np.outer(np.sin(u_blue), np.sin(v_blue))
+    z_blue = np.outer(np.ones_like(u_blue), np.cos(v_blue))
+    
+    ax.plot_surface(x_blue, y_blue, z_blue, rstride=4, cstride=4, color='lightblue', alpha=0.35, linewidth=0)
+    
+    return ax
+
+def plot_sphere():
     # Use matplotlib's default mathtext (no external LaTeX required)
     plt.rcParams.update({'font.size': 12})
     
@@ -31,11 +55,18 @@ def plot_sphere_and_points(save_path='bloch_custom.png', show=True):
     z = np.outer(np.ones_like(u), np.cos(v))
 
     ax.plot_surface(x, y, z, rstride=4, cstride=4, color='lightblue', alpha=0.35, linewidth=0)
+    
+    return ax
 
-    # Plot coordinate axes (unit)
-    ax.plot([0, 1], [0, 0], [0, 0], color='k', linewidth=1)
-    ax.plot([0, 0], [0, 1], [0, 0], color='k', linewidth=1)
-    ax.plot([0, 0], [0, 0], [0, 1], color='k', linewidth=1)
+def plot_sphere_and_points(save_path='bloch_custom.png', show=True, use_hemispheres=False):
+    # Use matplotlib's default mathtext (no external LaTeX required)
+    plt.rcParams.update({'font.size': 12})
+    
+    if use_hemispheres:
+        ax = plot_hemisperes()
+        save_path = ''.join(save_path.split('.')[:-1]) + '_hemispheres.png'
+    else:
+        ax = plot_sphere()
     
     deg_45 = math.pi / 4
     pi = math.pi
@@ -86,4 +117,4 @@ def plot_sphere_and_points(save_path='bloch_custom.png', show=True):
 
 
 if __name__ == '__main__':
-    plot_sphere_and_points()
+    plot_sphere_and_points(use_hemispheres=True)
